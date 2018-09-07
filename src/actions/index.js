eeb41eea13a41be72121d892b71f4e106145750a
vab <vincent.alibert@fullsave.com>
2018-09-07 16:11:09 +0200

    debut de redux

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const USER_SELECTED = "USER_SECLECTED";
+
+export function selectUser(user) {
+    
+    return {
+        type : USER_SELECTED,
+        payload : user
+    }
+    
+}
---
+++ src/reducers/reducer_users.js
@@ -0,0 +1,6 @@
+export default () => [
+    {name: 'Sarah Croche', id:1, role:'Root', active:'non'},
+    {name: 'Eleonore Labanquise', id:2, role:'User', active:'non'},
+    {name: 'Emma Karena', id:3, role:'User', active:'non'},
+    {name: 'Jean Bonnot', id:4, role:'Admin', active:'non'}
+]
